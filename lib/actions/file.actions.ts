a6fbc6cbeea079bfbb1f19b6f9d65ebbea1839a0
Bharath Kumar Murugan <bharathkumarmurugan2016@gmail.com>
2024-12-29 17:55:41 +0530

    updated with dashboard ui & other functionalities

--- lib/actions/file.actions.ts
+++ lib/actions/file.actions.ts
@@ -131,3 +131,40 @@ export const api_deleteFile = async ({ fileId, bucketFileId, path }: DeleteFileP
     handleError(error, "Failed to delete the file");
   }
 };
+
+export async function api_getTotalSpaceUsed() {
+  try {
+    const { databases } = await createAdminClient();
+    const currentUser = await api_getCurrentUser();
+
+    if (!currentUser) {
+      throw new Error("User not found");
+    }
+    const files = await databases.listDocuments(appwriteConfig.databaseId, appwriteConfig.filesCollectionId, [
+      Query.equal("owner", [currentUser.$id]),
+    ]);
+
+    const totalSpace = {
+      image: { size: 0, latestDate: "" },
+      document: { size: 0, latestDate: "" },
+      video: { size: 0, latestDate: "" },
+      audio: { size: 0, latestDate: "" },
+      other: { size: 0, latestDate: "" },
+      used: 0,
+      all: 2 * 1024 * 1024 * 1024 /* 2GB available bucket storage */,
+    };
+    files.documents.forEach((file) => {
+      const fileType = file.type as FileType;
+      totalSpace[fileType].size += file.size;
+      totalSpace.used += file.size;
+
+      if (!totalSpace[fileType].latestDate || new Date(file.$updatedAt) > new Date(totalSpace[fileType].latestDate)) {
+        totalSpace[fileType].latestDate = file.$updatedAt;
+      }
+    });
+
+    return parseStringify(totalSpace);
+  } catch (error) {
+    handleError(error, "Error calculating total space used:, ");
+  }
+}
