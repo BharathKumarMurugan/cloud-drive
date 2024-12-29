571d51b5e660ec299655e75cd9acdf22f903df98
Bharath Kumar Murugan <bharathkumarmurugan2016@gmail.com>
2024-12-29 15:19:46 +0530

    implmented sorting functionality

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { usePathname, useRouter } from "next/navigation";
+import { sortTypes } from "@/constants";
 
 const Sort = () => {
-  return (
-    <div>Sort</div>
-  )
-}
+  const router = useRouter();
+  const path = usePathname();
 
-export default Sort
+  const handlSort = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  };
+
+  return (
+    <Select onValueChange={handlSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className="sort-select-content">
+        {sortTypes.map((option) => (
+          <SelectItem key={option.label} value={option.value} className="shad-select-item">
+            {option.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default Sort;
